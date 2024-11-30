ecafeec898030d2ead71e67bc85eb7ba1959fde6
Hector Zea <hectorzea@Mac.fritz.box>
2024-11-30 14:44:57 +0100

    added shadcn and improvements

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      Hello FrontBuild
+    <div className="p-20">
+      <h2 className="text-2xl font-bold">Welcome back!</h2>
+      <p className="text-muted-foreground">
+        FrontBuild all task repository!
+      </p>
+      <div className="mt-8">
+        <h2 className="text-1xl font-bold">Frontend </h2>
+      </div>
+      <div className="mt-8">
+        <h2 className="text-1xl font-bold">Backend </h2>
+      </div>
     </div>
   );
 }
