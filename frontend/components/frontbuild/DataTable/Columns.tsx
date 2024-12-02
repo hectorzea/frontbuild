7a85e28631a0907a4559061f89b3cafd9ffe5132
Héctor Zea <zea3471@gmail.com>
2024-12-02 16:58:53 +0100

    change on width columnd

--- frontend/components/frontbuild/DataTable/Columns.tsx
+++ frontend/components/frontbuild/DataTable/Columns.tsx
@@ -40,7 +40,7 @@ export const columns: ColumnDef<Task>[] = [
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Task" />
     ),
-    cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
+    cell: ({ row }) => <div className="w-[95px]">{row.getValue("id")}</div>,
     enableSorting: false,
     enableHiding: false,
   },
