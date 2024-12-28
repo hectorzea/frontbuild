7e480b7528cbfb621e15016ad7510ac5cba01a14
Héctor Zea <zea3471@gmail.com>
2024-12-28 16:53:56 +0100

    Added comment

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { setUpMocks } from "../utils/setUpMocks"
 import { AppProps } from "next/app";
 
+// can this be changed to arrow functions?
 export default function App({ Component, pageProps }: AppProps) {
   {
     const [isMswReady, setIsMswReady] = useState<boolean>(false);
