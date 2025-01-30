3a3b955e32fcdc28791afde5cb1993ca78f49190
Héctor Zea <zea3471@gmail.com>
2025-01-30 16:41:42 +0100

    updates on types on swagger

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -1,18 +1,8 @@
 import swaggerJSDoc from "swagger-jsdoc";
-import mongooseToSwagger from "mongoose-to-swagger";
-import Task, { ITask } from "../models/taskModel";
-import Label from "../models/labelModel"
-import Status from "../models/statusModel"
-import Priority from "../models/priorityModel"
 import swaggerAutogen from "swagger-autogen";
 
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || "http://localhost";
-
-const taskSchema = mongooseToSwagger(Task, { omitMongooseInternals: true });
-const labelSchema = mongooseToSwagger(Label);
-const statusSchema = mongooseToSwagger(Status);
-const prioritySchema = mongooseToSwagger(Priority);
 
 const swaggerDoc = {
     openapi: '3.0.0',
