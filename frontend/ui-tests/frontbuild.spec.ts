e4f845c00c331521b92c805682c14d257d9502c9
Héctor Zea <zea3471@gmail.com>
2025-02-23 13:19:23 +0100

    added basic flow on ui-tests

--- frontend/ui-tests/frontbuild.spec.ts
+++ frontend/ui-tests/frontbuild.spec.ts
@@ -1,21 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-// test('has title', async ({ page }) => {
-//   await page.goto('http://localhost:3000/');
-
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Frontbuild/);
-// });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('http://localhost:3000/');
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('button', { name: 'Add Task' })).toBeVisible();
-// });
-
-test('add task', async ({ page }) => {
+test('App basic flow', async ({ page }) => {
   await page.goto('http://localhost:3000/');
+
+  await expect(page).toHaveTitle(/Frontbuild/);
   await page.getByTestId('add-task-button').click();
   await page.getByRole('textbox', { name: 'Title' }).fill('hola2');
 
