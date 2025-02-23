3eb74ebcb22d887d373632c8792bcba0c35d064a
Héctor Zea <zea3471@gmail.com>
2025-02-23 18:31:10 +0100

    updated pipeline

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://127.0.0.1:3000',
+    port: 3000,
     env: {
       ENABLE_MSW: "true",
     },
