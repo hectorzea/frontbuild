485436af254c6682719cac446a76665a86a47152
Héctor Zea <zea3471@gmail.com>
2025-02-23 18:14:40 +0100

    changes on config timeout

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -73,8 +73,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    cwd: '../', 
     port: 3000,
+    timeout: 60 * 1000,
     env: {
       ENABLE_MSW: "true",
     },
