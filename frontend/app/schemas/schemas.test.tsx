f2c25de64a910929041004dbb4cce73580fd6fd6
Héctor Zea <zea3471@gmail.com>
2025-02-27 21:08:14 +0100

    updated test

--- frontend/app/schemas/schemas.test.tsx
+++ frontend/app/schemas/schemas.test.tsx
@@ -4,11 +4,8 @@ test('Test schema with all valid data ', async () => {
     expect(() => taskSchema.parse({ title: 'do something', status: 'todo', label: 'epic', priority: 'low' })).not.toThrow();
 });
 
-test('Test schema with required title', async () => {
-    expect(() => taskSchema.parse({ title: '' })).toThrow("Title is required");
-});
-
 test('Test schema with invalid statuses', async () => {
+    expect(() => taskSchema.parse({ title: '' })).toThrow("Title is required");
     expect(() => taskSchema.parse({ title: 'do something', status: 'todos-s', label: 'something', priority: 'low' })).toThrow("Invalid status");
     expect(() => taskSchema.parse({ title: 'do something', status: 'todo', label: 'something', priority: 'low' })).toThrow("Invalid label");
     expect(() => taskSchema.parse({ title: 'do something', status: 'todo', label: 'epic', priority: 'standar' })).toThrow("Invalid priority");
