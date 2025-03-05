f0a92a64aa05eeabbd273c063da51a179a0df331
Héctor Zea <zea3471@gmail.com>
2025-03-05 20:00:00 +0100

    domain

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: process.env.TEST_API_URL || 'http://localhost:3000',
+    baseURL: process.env.TEST_API_URL || 'http://localhost:3222',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
