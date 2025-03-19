f0791d5f1a0acb0ef458af1414a94ab00bdb22af
Héctor Zea <zea3471@gmail.com>
2025-03-19 17:46:20 +0100

    updated generateStaticParams

--- frontend/app/profile/interviews/[id]/page.tsx
+++ frontend/app/profile/interviews/[id]/page.tsx
@@ -1,10 +1,7 @@
-// Return a list of `params` to populate the [slug] dynamic segment
 export async function generateStaticParams() {
     return [{ slug: "test" }, { slug: "test" }];
 }
 
-// Multiple versions of this page will be statically generated
-// using the `params` returned by `generateStaticParams`
 export default async function Page({
     params,
 }: {
