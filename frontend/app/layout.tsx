158e0864137bef9e507a9dbc4dc605bbcf16b388
Héctor Zea <zea3471@gmail.com>
2025-03-23 11:02:59 +0100

    ADJUSTED ORDER

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import type { Metadata } from "next";
 import { StoreProvider } from "./StoreProvider";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
