f210ca607c8e5d67b04c00abe817b87d41e0d086
Héctor Zea <zea3471@gmail.com>
2025-05-26 14:12:40 +0200

    updated ui

--- frontend/app/[lang]/page.tsx
+++ frontend/app/[lang]/page.tsx
@@ -1,10 +1,12 @@
-import { redirect } from 'next/navigation';
-export default async function Index() {
-    redirect('/profile');
-    // const { t } = await getT('common');
-    // return (
-    //     <div>
-    //         {t('accept')}
-    //     </div>
-    // );
+import { getT } from "@/app/i18n";
+import Link from "next/link";
+
+export default async function ProfileIndexPage() {
+    const { t } = await getT('main');
+    return (
+        <div className="flex flex-col items-center justify-center h-screen text-center space-y-6">
+            <p>{t('title')}</p>
+            <Link href={`/profile`} className="text-blue-500 hover:underline">{t('link')}</Link>
+        </div>
+    );
 }
