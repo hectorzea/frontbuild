f388b34d6516f2125fc27b7987aa94a309be5faa
Héctor Zea <zea3471@gmail.com>
2025-05-26 14:47:46 +0200

    test check subpatch 54

--- frontend/app/[lang]/profile/layout.tsx
+++ frontend/app/[lang]/profile/layout.tsx
@@ -1,7 +1,7 @@
 import { Separator } from "@/components/ui/separator"
 import { SidebarNav } from "@/components/frontbuild/SidebarNav"
 import { HamburgerMenu } from "@/components/frontbuild/HamburgerMenu"
-import { languages } from "@/app/i18n/settings"
+// import { languages } from "@/app/i18n/settings"
 import { getT } from "@/app/i18n"
 
 
