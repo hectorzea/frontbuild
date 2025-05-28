f5b56c1e360a2b754b5e447b131b38042298e0cc
Héctor Zea <zea3471@gmail.com>
2025-05-28 17:47:18 +0200

    more fixes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,3 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import { TaskDashboard } from '../components/frontbuild/TaskDashboard';
-import { setUpMocks } from '@/app/mocks/setupMocks';
-import { Loading } from '@/components/frontbuild/Loading';
-
 export default function IndexPage() {
-  const [isMswReady, setIsMswReady] = useState<boolean>(false);
 
-  useEffect(() => {
-    const initializeMocking = async () => {
-      if (process.env.NEXT_PUBLIC_ENABLE_MSW === "true") {
-        await setUpMocks();
-      }
-      setIsMswReady(true);
-    };
-    initializeMocking();
-  }, []);
-
-  if (!isMswReady) {
-    return <div className='flex justify-center items-center h-screen'><Loading /></div>;
-  }
-
-  return <TaskDashboard title={'jaja'}/>;
 }
