8e317f4509df2ec711d3b0446195ebbb46e04868
Héctor Zea <zea3471@gmail.com>
2025-05-29 17:58:00 +0200

    removed unused prop

--- frontend/app/__tests__/locale-home-page.test.tsx
+++ frontend/app/__tests__/locale-home-page.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react'
 import HomePage from '@/app/[locale]/page'
-import en from '@/i18n/messages/en.json';
 
 describe('Page', () => {
     it('renders a heading', () => {
