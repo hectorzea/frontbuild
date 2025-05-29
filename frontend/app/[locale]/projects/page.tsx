3a3d23ba45b9279d8d1f150c45b72b180a049551
Héctor Zea <zea3471@gmail.com>
2025-05-29 18:16:17 +0200

    more tests

--- frontend/app/[locale]/projects/page.tsx
+++ frontend/app/[locale]/projects/page.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
-export default async function ProjectsPage() {
+export default function ProjectsPage() {
     return (
-        <>
+        <div data-testid="projects-page">
             <p>Proyectos</p>
 
             <Link href={`/projects/tasks`}>Tareas</Link>
-        </>
+        </div>
     );
 }
