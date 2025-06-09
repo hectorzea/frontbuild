8a35da90abb384ad3f865f9672203fc2b8aa32fa
Héctor Zea <zea3471@gmail.com>
2025-06-09 19:40:26 +0200

    changes

--- frontend/hooks/__tests__/useMsw.test.ts
+++ frontend/hooks/__tests__/useMsw.test.ts
@@ -8,12 +8,20 @@ jest.mock('@/app/mocks/setupMocks', () => ({
 }));
 
 describe('useMsw', () => {
-    it('should initialize with isMswReady in false', async () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        process.env.NEXT_PUBLIC_ENABLE_MSW = 'false';
+    });
+    it('should initialize the mock service worker', async () => {
         process.env.NEXT_PUBLIC_ENABLE_MSW = 'true';
         const { result } = renderHook(() => useMsw());
         await waitFor(() => {
             expect(result.current).toBe(true);
             expect(SetupMocks.setUpMocks).toHaveBeenCalled();
         });
+    });
+
+    it('should NOT call setupMocks when NEXT_PUBLIC_ENABLE_MSW is false', async () => {
+        expect(SetupMocks.setUpMocks).not.toHaveBeenCalled();
     });
 })
