d877c63c34663fcb0f3a3610539f0a7681354b99
Héctor Zea <zea3471@gmail.com>
2025-06-11 14:54:34 +0200

    test path

--- frontend/components/frontbuild/DownloadButton/index.tsx
+++ frontend/components/frontbuild/DownloadButton/index.tsx
@@ -4,17 +4,14 @@ import { Download } from 'lucide-react'
 import React from 'react'
 
 const DownloadCVButton = () => {
-    const downloadCV = () => {
-        const link = document.createElement('a');
-        link.href = '/files/CV.pdf'; // Adjust the path to your CV file
-        link.download = 'CV_Hector_Zea.pdf'; // The name of the downloaded file
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
     return (
-        <Button size={'lg'} data-testid={'add-task-button'} className="w-full sm:w-auto mt-2 sm:mt-0" onClick={downloadCV}>
-            <Download /> Download my CV
+        <Button size={'lg'} data-testid={'add-task-button'} className="w-full sm:w-auto mt-2 sm:mt-0">
+            <Download />
+            <a
+                href="/files/CV.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+            >Download My CV</a>
         </Button>
     )
 }
