4c328c18a581237a1c7bdafdb756b0f2b05cb12c
Héctor Zea <zea3471@gmail.com>
2025-06-11 15:13:19 +0200

    updated link

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -8,13 +8,14 @@ export default function HomePage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen text-center space-y-6 mx-4">
             <p>{t('title')}</p>
-            <Button size={'lg'} data-testid={'add-task-button'} className="w-full sm:w-auto mt-2 sm:mt-0">
+            <Button size={'lg'} data-testid={'download-cv-button'} className="w-full sm:w-auto mt-2 sm:mt-0">
                 <Download />
-                <a
+                <Link
                     href="/files/CV.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
-                >Download My CV</a>
+                    aria-label="Downlod CV"
+                >Download My CV</Link>
             </Button>
             <Link href={`/profile/skills`} className="text-blue-500 hover:underline">{t('profile')}</Link>
         </div>
