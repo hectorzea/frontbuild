667fb3cf2eddfb4423b1e10162759edfbe1806cf
Héctor Zea <zea3471@gmail.com>
2025-06-20 16:21:36 +0200

    adjusted concept

--- frontend/components/frontbuild/LinkForm/index.tsx
+++ frontend/components/frontbuild/LinkForm/index.tsx
@@ -17,6 +17,7 @@ import axios from "axios"
 import { useState } from "react"
 import Link from "next/link"
 import { ArrowBigRight } from "lucide-react"
+import { set } from "ui-box/dist/src/cache"
 
 const FormSchema = z.object({
     linkedInJobUrl: z.string().min(2, {
@@ -26,21 +27,6 @@ const FormSchema = z.object({
 
 export function InputForm() {
     const [data, setData] = useState<any>(null);
-    const extractJson = (textWithDelimiters: any) => {
-        const jsonString = textWithDelimiters
-            .replace(/^```json\n/, '')
-            .replace(/\n```$/, '')
-            .trim();
-
-        try {
-            // Intentar parsear el JSON
-            const jsonObject = JSON.parse(jsonString);
-            return jsonObject;
-        } catch (error) {
-            console.error('Error parsing JSON:', error);
-            return null; // O lanzar el error, según lo que prefieras
-        }
-    }
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
         defaultValues: {
@@ -49,28 +35,11 @@ export function InputForm() {
     })
 
     async function onSubmit(data: z.infer<typeof FormSchema>) {
-        console.log('API call initiated');
-        console.log(data.linkedInJobUrl);
-        // const jobDataStructure = "respondeme con un json con esta estructura de esta publicacion de trabajo con lo mas importante pero solo con esta estructura "
-        const jobResponse = await axios.get(`http://localhost:3000/job-extractor?url=${data.linkedInJobUrl}`);
-        const jobRawHtml = jobResponse.data?.content;
-        const jobDataStructure = "Dado este HTML completo de una URL de publicacion de trabajo, extrae la informacion mas importante y devuelve un JSON";
-        const jsonConsiderations = "los nombres de los atributos del json tienen relacion con la informacion, tomalo como referencia tambien para el calculo final), retorna null los atributos que no puedas llenar. el campo salaryRange que sea en euros el rango. el campo considerations tienes que llenarlo tu con lo que crees que es importante investigar / aprender para encajar mejor en el trabajo. el campo jobDescription que sea una informacion mas resumida (corta) de lo que hacen, a continuacion el JSON:"
-        const jobJsonStructure = "{allowRelocation:boolean, yearsOfExperience:string ,salaryRange:string, recruitmentProcessSteps:string[], jobLink:string, companyName:string, considerations:string, hardSkills:string[], jobDescription:string, jobTitle:string, location:string, softSkills:string[], teamSize:number, workEnvironment:string";
-        //todo esto seria una api call al scrapper asincrona que devolveria la info en exto
-        // const jobDescription = "jobdescription";
-        const textContent = `${jobDataStructure} ${jsonConsiderations} ${jobJsonStructure} HTML: ${jobRawHtml}`;
         try {
-            const ai = new GoogleGenAI({ apiKey: "" });
-            const response = await ai.models.generateContent({
-                model: "gemini-2.0-flash",
-                contents: textContent
-            });
-            const jobDetails = extractJson(response.text);
-            console.log(jobDetails)
-            setData(jobDetails);
-        }
-        catch (error) {
+            console.log('API call initiated');
+            const jobResponse = await axios.post(`http://localhost:3000/ai/process-job`, { linkedinJobUrl: data.linkedInJobUrl });
+            setData(jobResponse.data);
+        } catch (error) {
             console.error('Error calling google api cloud:', error);
         }
     }
@@ -147,9 +116,11 @@ export function InputForm() {
                         ))}
                     </ul>
                     <h3 className="text-2xl font-semibold mt-3">
-                        Considerations about the job
+                        Considerations about the job (IA)
                     </h3>
-                    <div>{data?.considerations}</div>
+                    <div>How to fit better?:{data?.considerations}</div>
+                    <div>Match %: {data?.matchPercentage}</div>
+                    <div>Match Reasoning: {data?.matchReasoning}</div>
                 </div> : <div className="mt-4">No data submitted yet</div>}
         </>
     )
