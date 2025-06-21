92b837852f1d100c5bc67889068b43ede4348d0d
Héctor Zea <zea3471@gmail.com>
2025-06-21 11:54:35 +0200

    fixes

--- frontend/app/setupTests.ts
+++ frontend/app/setupTests.ts
@@ -1,24 +1,28 @@
-import '@testing-library/jest-dom';
-import '@/app/mocks/matchMedia.mock';
-import { loadEnvConfig } from '@next/env';
-import en  from '@/i18n/messages/en.json';
+import "@testing-library/jest-dom";
+import "@/app/mocks/matchMedia.mock";
+import { loadEnvConfig } from "@next/env";
+import en from "@/i18n/messages/en.json";
 
-type ValidNamespace = 'HomePage';
-jest.mock('next/navigation', () => jest.requireActual('next-router-mock'))
-jest.mock('next-intl', () => {
-    return {
-        useTranslations: jest.fn().mockImplementation((namespace: ValidNamespace) => {
-            return (key: string) => {
-                const translations = {
-                    en
-                };
-                const homePageTranslations = translations['en'][namespace]
-                return (homePageTranslations && typeof key === 'string' && key in homePageTranslations)
-                    ? homePageTranslations[key as keyof typeof homePageTranslations] //assertion
-                    : `Missing translation: ${namespace}.${key}`;
-            };
-        }),
-    };
+type ValidNamespace = "HomePage";
+jest.mock("next/navigation", () => jest.requireActual("next-router-mock"));
+jest.mock("next-intl", () => {
+  return {
+    useTranslations: jest
+      .fn()
+      .mockImplementation((namespace: ValidNamespace) => {
+        return (key: string) => {
+          const translations = {
+            en,
+          };
+          const homePageTranslations = translations["en"][namespace];
+          return homePageTranslations &&
+            typeof key === "string" &&
+            key in homePageTranslations
+            ? homePageTranslations[key as keyof typeof homePageTranslations] //assertion
+            : `Missing translation: ${namespace}.${key}`;
+        };
+      }),
+  };
 });
 
 const projectDir = process.cwd();
