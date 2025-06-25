0cd3ce56ca94c01c96dda5ffb329539a89fbcb5c
Héctor Zea <zea3471@gmail.com>
2025-06-25 22:35:53 +0200

    added improvements and new pdf's

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -1,27 +1,25 @@
 import { Button } from "@/components/ui/button";
-import { Download } from "lucide-react";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 export default function HomePage() {
-  //todo, check the best way to have a button + link shadcn practices
   const t = useTranslations("HomePage");
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center space-y-6 mx-4">
       <p>{t("title")}</p>
       <Button
+        asChild
         size={"lg"}
         data-testid={"download-cv-button"}
-        className="w-full sm:w-auto mt-2 sm:mt-0"
+        className="sm:w-auto mt-2 sm:mt-0"
       >
-        <Download />
         <Link
-          href="/files/CV.pdf"
+          href={`/files/${t("cv")}.pdf`}
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Downlod CV"
         >
-          Download My CV
+          {t("cvLinkName")}
         </Link>
       </Button>
       <Link href={`/profile/skills`} className="text-blue-500 hover:underline">
