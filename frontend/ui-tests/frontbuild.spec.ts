884cc3a47f53cac9e7ebd70f7e180b45d5126228
Héctor Zea <zea3471@gmail.com>
2025-07-08 16:31:55 +0200

    updated snapshots

--- frontend/ui-tests/frontbuild.spec.ts
+++ frontend/ui-tests/frontbuild.spec.ts
@@ -2,11 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("Home page should match snapshot", async ({ page }) => {
   await page.goto("/");
-  await expect(
-    page.getByText(
-      /Frontbuild is a repository which contains my personal projects/i,
-    ),
-  ).toBeVisible();
+  await expect(page.getByText(/Hi, my name is Hector Zea,/i)).toBeVisible();
   const screenshot = await page.screenshot();
   expect(screenshot).toMatchSnapshot("home-page.png", { maxDiffPixels: 10000 });
 });
@@ -15,7 +11,7 @@ test("App basic flow", async ({ page }) => {
   await page.goto("/projects/tasks");
 
   await expect(page).toHaveTitle("Frontbuild");
-  await expect(page.getByText(/frontbuild is a repository/i)).toBeVisible();
+  await expect(page.getByText(/Hi, my name is Hector Zea,/i)).toBeVisible();
 
   await page.getByTestId("add-task-button").click();
   await page
