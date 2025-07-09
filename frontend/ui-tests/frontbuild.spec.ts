769b09ddc8a3f17294912c0027852015a3dfbcb6
Héctor Zea <zea3471@gmail.com>
2025-07-09 15:52:28 +0200

    updates

--- frontend/ui-tests/frontbuild.spec.ts
+++ frontend/ui-tests/frontbuild.spec.ts
@@ -1,17 +1,21 @@
 import { test, expect } from "@playwright/test";
 
-test("Home page should match snapshot", async ({ page }) => {
+test("Fronbuild Quality Tests // Home Page (Snapshot Test)", async ({
+  page,
+}) => {
   await page.goto("/");
   await expect(page.getByText(/Hi, my name is Hector Zea,/i)).toBeVisible();
   const screenshot = await page.screenshot();
   expect(screenshot).toMatchSnapshot("home-page.png", { maxDiffPixels: 10000 });
 });
 
-test("App basic flow", async ({ page }) => {
+test("Fronbuild Flow // Tasks", async ({ page }) => {
   await page.goto("/projects/tasks");
 
-  await expect(page).toHaveTitle("Frontbuild");
-  await expect(page.getByText(/Hi, my name is Hector Zea,/i)).toBeVisible();
+  await expect(page.getByText(/Easy Task Creator/i)).toBeVisible();
+  await expect(
+    page.getByText(/Task creator using tanstack react-table/i)
+  ).toBeVisible();
 
   await page.getByTestId("add-task-button").click();
   await page
