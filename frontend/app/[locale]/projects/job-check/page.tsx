0019229bd87a8045250aba160a5e2733d45e4ff1
Héctor Zea <zea3471@gmail.com>
2025-07-18 20:20:57 +0200

    changes on job-checker

--- frontend/app/[locale]/projects/job-check/page.tsx
+++ frontend/app/[locale]/projects/job-check/page.tsx
@@ -4,8 +4,10 @@ import React from "react";
 export default function ProjectsPage() {
   return (
     <div data-testid="projects-page" className="flex flex-col p-4">
-      <p>Job linter</p>
-      <JobCheckForm />
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <p>Job linter</p>
+        <JobCheckForm />
+      </div>
     </div>
   );
 }
