85fbd0d97ffc6ce61364aa51c3fe45d2b6f9fd49
Héctor Zea <zea3471@gmail.com>
2025-07-23 21:51:06 +0200

    removed testid check

--- frontend/app/__tests__/job-check-page.test.tsx
+++ frontend/app/__tests__/job-check-page.test.tsx
@@ -6,9 +6,6 @@ describe("Job Check", () => {
     render(<JobCheckPage />);
     expect(screen.getByText("Job linter")).toBeInTheDocument();
     expect(
-      screen.getByTestId("no-data-job-check-container")
-    ).toBeInTheDocument();
-    expect(
       screen.getByTestId("submit-button-job-check-form")
     ).toBeInTheDocument();
   });
