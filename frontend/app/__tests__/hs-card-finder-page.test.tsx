70b230cf7cbf42165ac52e65f5273d7ed1feb0fb
Héctor Zea <zea3471@gmail.com>
2025-07-25 13:29:02 +0200

    normal task api url

--- frontend/app/__tests__/hs-card-finder-page.test.tsx
+++ frontend/app/__tests__/hs-card-finder-page.test.tsx
@@ -4,6 +4,6 @@ import HearthstoneCardFinder from "@/app/[locale]/projects/hs-card-finder/page";
 describe("Hearthstone Card Finder", () => {
   it("renders page", () => {
     render(<HearthstoneCardFinder />);
-    expect(screen.getByText("HS CARD FINDER")).toBeInTheDocument();
+    expect(screen.getByTestId("hs-card-finder-page")).toBeInTheDocument();
   });
 });
