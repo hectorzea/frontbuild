42ae4ac51280b88f1a4a9112cbc5d2e9a9e7e938
Héctor Zea <zea3471@gmail.com>
2025-08-05 16:50:36 +0200

    added table for discards

--- frontend/components/frontbuild/MulliganTable/index.tsx
+++ frontend/components/frontbuild/MulliganTable/index.tsx
@@ -4,23 +4,39 @@ import { DataTable } from "../DataTable";
 import { mulliganColumns } from "../DataTable/Columns";
 
 export function MulliganTable() {
-  const { data } = useGetMulliganCardsQuery({
+  const { data: initialCards } = useGetMulliganCardsQuery({
     classId: "DEATHKNIGHT",
     type: "initial",
   });
-  if (!data) {
+
+  const { data: discardedCards } = useGetMulliganCardsQuery({
+    classId: "DEATHKNIGHT",
+    type: "discarded",
+  });
+  if (!initialCards || !discardedCards) {
     return undefined;
   }
   return (
     <>
-      <p>Mulligan winrate</p>
-      <p>Partidas ganadas por carta individual</p>
-      <DataTable
-        data={data}
-        toolbarEnabled={false}
-        columns={mulliganColumns}
-        data-testid={"mulligan-cards-table"}
-      />
+      <div>
+        <p className="text-xl">MULLIGAN WINRATE STATS</p>
+        <p className="mt-3">Partidas ganadas por carta individual (INICIAL)</p>
+        <DataTable
+          data={initialCards}
+          toolbarEnabled={false}
+          columns={mulliganColumns}
+          data-testid={"mulligan-cards-table"}
+        />
+      </div>
+      <div className="mt-5">
+        <p>Partidas ganadas por carta individual (DESCARTADAS / MULLIGAN)</p>
+        <DataTable
+          data={discardedCards}
+          toolbarEnabled={false}
+          columns={mulliganColumns}
+          data-testid={"mulligan-cards-table"}
+        />
+      </div>
     </>
   );
 }
