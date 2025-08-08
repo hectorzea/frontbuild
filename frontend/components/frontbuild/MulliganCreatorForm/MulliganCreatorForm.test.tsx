4e5fde883ba987131e56e5561876a35e8a9ae959
Héctor Zea <zea3471@gmail.com>
2025-08-08 16:58:32 +0200

    added

--- frontend/components/frontbuild/MulliganCreatorForm/MulliganCreatorForm.test.tsx
+++ frontend/components/frontbuild/MulliganCreatorForm/MulliganCreatorForm.test.tsx
@@ -3,8 +3,10 @@ import { screen } from "@testing-library/react";
 import { MulliganCreatorForm } from ".";
 import { renderWithProviders } from "@/app/test-utils";
 import ModalWrapper from "../Modal/ModalWrapper";
+import userEvent from "@testing-library/user-event";
 
-test("Loads <TaskForm /> and check for main elements", async () => {
+test("Loads <MulliganCreatorForm /> check elements and create a match", async () => {
+  const user = userEvent.setup();
   renderWithProviders(
     <ModalWrapper route={"route"}>
       <MulliganCreatorForm />
@@ -23,4 +25,10 @@ test("Loads <TaskForm /> and check for main elements", async () => {
   expect(
     await screen.findByTestId("close-button-card-match-form")
   ).toBeInTheDocument();
+
+  const input = screen.getByTestId("card-match-url-input-field");
+
+  await user.type(input, "https://hsreplay.net/replay/UEpCuDvFktfgBpq6AjH4rP");
+
+  await user.click(screen.getByTestId("submit-button-card-match-result"));
 });
