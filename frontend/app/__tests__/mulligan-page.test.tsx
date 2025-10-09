8d5e9b421e183d6bea5d4d49e4e4063a5ba9e31d
Héctor Zea <zea3471@gmail.com>
2025-10-09 20:27:07 +0200

    udpdated tests

--- frontend/app/__tests__/mulligan-page.test.tsx
+++ frontend/app/__tests__/mulligan-page.test.tsx
@@ -1,5 +1,4 @@
 import { screen, waitFor } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
 import MulliganPage from "@/app/[locale]/projects/hs-card-search/mulligan/page";
 import { renderWithProviders } from "../test-utils";
 
@@ -7,11 +6,11 @@ describe("Mulligan Page - ", () => {
   it("Render full UI", async () => {
     renderWithProviders(<MulliganPage />);
 
-    expect(screen.getByTestId("hs-mulligan-creator-page"));
-
     await waitFor(() => {
-      expect(screen.getByTestId("mulligan-table-title"));
-      expect(screen.getByTestId("add-new-match-mulligan"));
+      expect(screen.getByTestId("mulligan-table-title")).toBeInTheDocument();
+      expect(
+        screen.getByRole("link", { name: "Add new match" })
+      ).toHaveAttribute("href", "/projects/hs-card-search/mulligan/new-match");
     });
   });
 });
