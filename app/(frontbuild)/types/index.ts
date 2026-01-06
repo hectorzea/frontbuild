7dad2f79ca0770d00ed8653886a010da3b527484
Héctor Zea <zea3471@gmail.com>
2026-01-06 13:54:42 +0100

    comment

--- app/(frontbuild)/types/index.ts
+++ app/(frontbuild)/types/index.ts
@@ -15,7 +15,7 @@ export const priorityIconMap = {
   low: ArrowDown,
 };
 
-//todo ver si generar un record aca
+//todo ver si generar un record acaa
 export type Priority = keyof typeof priorityIconMap;
 export type PriorityIcon = (typeof priorityIconMap)[Priority];
 
