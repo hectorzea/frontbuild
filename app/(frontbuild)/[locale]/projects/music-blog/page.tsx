3bf6f9afd57bb2ad38835bd4448f04c934239a1f
Héctor Zea <zea3471@gmail.com>
2026-01-16 14:38:53 +0100

    added first songs collection TODO media

--- app/(frontbuild)/[locale]/projects/music-blog/page.tsx
+++ app/(frontbuild)/[locale]/projects/music-blog/page.tsx
@@ -1,8 +1,35 @@
+import { getPayload } from "payload";
+import configPromise from "@payload-config";
+
 //TODO: Find about SSG + Payload CMS
-export default function MusicBlogPage() {
+export default async function MusicBlogPage() {
+  const payload = await getPayload({ config: configPromise });
+  const songs = await payload.find({
+    collection: "songs",
+    //TODO in depth soon
+    // depth: 1,
+    // limit: 12,
+    // overrideAccess: false,
+    // select: {
+    //   title: true,
+    //   slug: true,
+    //   categories: true,
+    //   meta: true,
+    // },
+  });
+
   return (
-    <div data-testid="music-blog-page" className="flex">
-      <p>Hi Blog</p>
+    <div data-testid="music-blog-page" className="flex flex-col">
+      <p>My Most Favorite Songs</p>
+      <div className="flex gap-5">
+        {songs.docs?.map((song, index) => (
+          <div className="flex flex-col gap-2" key={`song-${index}`}>
+            <p>{song.id}</p>
+            <p>{song.songTitle}</p>
+            <p>{song.artist}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- payload.config.ts
+++ payload.config.ts
@@ -2,13 +2,14 @@
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { buildConfig } from "payload";
+import { Songs } from "./collections/Songs";
 
 export default buildConfig({
   // If you'd like to use Rich Text, pass your editor here
   editor: lexicalEditor(),
 
   // Define and configure your collections in this array
-  collections: [],
+  collections: [Songs],
 
   // Your Payload secret - should be a complex and secure string, unguessable
   secret: process.env.PAYLOAD_SECRET || "",
@@ -16,4 +17,5 @@ export default buildConfig({
   db: mongooseAdapter({
     url: process.env.DATABASE_URL || "",
   }),
+  //TODO volver a usar sharp para media collection
 });
