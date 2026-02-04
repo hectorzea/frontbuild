169db9f22acf1f5ec3e99a16c3d43397aabed6d1
Héctor Zea <zea3471@gmail.com>
2026-02-04 20:10:29 +0100

    added default.test.tsx

--- app/(tasks)/@modal/__tests__/default.test.tsx
+++ app/(tasks)/@modal/__tests__/default.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import DefaultPage from "@/app/(tasks)/@modal/default";
-describe("Music Blog Page", () => {
-  it("is rendering correctly", async () => {
+describe("@modal", () => {
+  it("is rendering Default Page correctly", async () => {
     const { container } = render(DefaultPage());
     expect(container.firstChild).toBeNull();
   });
