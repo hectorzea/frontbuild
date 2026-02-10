0846b3d17b81194455305206cc33c724783fa887
Héctor Zea <zea3471@gmail.com>
2026-02-10 13:19:30 -0300

    ongoing test

---
+++ app/(tasks)/@modal/__tests__/add-task-intercepted.test.tsx
@@ -0,0 +1,23 @@
+import { screen, waitFor } from "@testing-library/react";
+import { renderWithProviders } from "@/app/(frontbuild)/test-utils";
+import InterceptedPage from "@/app/(tasks)/@modal/(.)tasks/new/page";
+import userEvent from "@testing-library/user-event";
+
+//TODO FINISH THIS TEST PASAR DE SYNC A ASYNC INTERCEPTED PAGE ERROR
+describe("Add new task page", () => {
+  it("renders page and submit a task creation without filling fields - ERROR", async () => {
+    const user = userEvent.setup();
+    renderWithProviders(<InterceptedPage />);
+
+    expect(screen.getByTestId("task-form-title")).toBeInTheDocument();
+    expect(screen.getByTestId("task-form-status")).toBeInTheDocument();
+    expect(screen.getByTestId("task-form-label")).toBeInTheDocument();
+    expect(screen.getByTestId("task-form-priority")).toBeInTheDocument();
+
+    await user.click(screen.getByTestId("task-form-submit-button"));
+
+    await waitFor(() => {
+      expect(screen.getByText("Title is required")).toBeInTheDocument();
+    });
+  });
+});
