0b45ec0068343d3bd4246afb91f2af13e7340714
Héctor Zea <zea3471@gmail.com>
2026-03-26 21:53:11 -0300

    iteration

--- ui-tests/songs.spec.ts
+++ ui-tests/songs.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 test("Music Blog", async ({ page }) => {
   await page.goto("/songs");
   await expect(page.getByText(/My Most Favorite Song/i)).toBeVisible();
-  await expect(page.getByText(/Crystal Glow/i)).toBeVisible();
+  await expect(page.getByText(/Atrevete/i)).toBeVisible();
 });
