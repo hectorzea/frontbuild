05be9afac5ec151ddf0615f7b7f232bd1d4b7b0d
Héctor Zea <zea3471@gmail.com>
2026-03-26 21:42:59 -0300

    iteration

--- app/(music-blog)/__tests__/music-blog.test.tsx
+++ app/(music-blog)/__tests__/music-blog.test.tsx
@@ -29,9 +29,29 @@ beforeEach(() => {
   });
 });
 
-describe("Music Blog Page", () => {
+// describe("Music Blog Page", () => {
+//   it("is rendering correctly", async () => {
+//     render(await SongsPage());
+//     expect(screen.getByTestId("music-blog-page")).toBeInTheDocument();
+//   });
+// });
+
+describe("Page Song By Id", () => {
+  const originalEnv = process.env;
+
+  afterEach(() => {
+    process.env = originalEnv;
+  });
+
   it("is rendering correctly", async () => {
     render(await SongsPage());
     expect(screen.getByTestId("music-blog-page")).toBeInTheDocument();
+  });
+
+  it("should do something when IS_E2E is true", async () => {
+    process.env.NEXT_PUBLIC_IS_E2E = "true";
+    render(await SongsPage());
+    expect(screen.getByTestId("music-blog-page")).toBeInTheDocument();
+    expect(screen.getByText("Atrevete")).toBeInTheDocument();
   });
 });
