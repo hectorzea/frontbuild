7e1fe855cee827f5078f99889bbe55fab1d2060d
Héctor Zea <zea3471@gmail.com>
2026-04-07 21:28:39 -0300

    token fix name

--- mocks/auth/data.ts
+++ mocks/auth/data.ts
@@ -3,7 +3,7 @@ import { LoginMockScenarios } from "./types";
 export const loginMockScenarios: LoginMockScenarios = {
   "successUser@test.abc": {
     status: 200,
-    response: { acessToken: "fake-token", user: "successUser@test.abc" },
+    response: { accessToken: "fake-token", user: "successUser@test.abc" },
   },
   "errorUser@test.abc": {
     status: 403,
