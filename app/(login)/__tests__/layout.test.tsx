b4a655a7fd3c0766429329059e56d74bdd44840f
Héctor Zea <zea3471@gmail.com>
2026-04-11 09:47:59 -0300

    mock

--- app/(login)/__tests__/layout.test.tsx
+++ app/(login)/__tests__/layout.test.tsx
@@ -1,6 +1,20 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import RootLayout from "../layout";
 
+//todo workaround for checks
+jest.mock("next/navigation", () => {
+  return {
+    useRouter: () => ({
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+    }),
+    usePathname: () => ({
+      startsWith: () => {},
+    }),
+  };
+});
+
 describe("Login Layout", () => {
   it("renders the layout", async () => {
     render(<RootLayout>Login</RootLayout>);
