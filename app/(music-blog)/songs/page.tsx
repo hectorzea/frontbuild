039c02f68a94eb520eebcb3396d47acfd0c62b2b
Héctor Zea <zea3471@gmail.com>
2026-04-11 21:03:43 -0300

    fixed tests

--- app/(music-blog)/songs/page.tsx
+++ app/(music-blog)/songs/page.tsx
@@ -2,6 +2,7 @@ import { getPayload } from "payload";
 import configPromise from "@payload-config";
 import Image from "next/image";
 import Link from "next/link";
+import { Media } from "@/payload-types";
 
 const getMockSongs = () => ({
   docs: [
@@ -32,25 +33,29 @@ export default async function SongsPage() {
         My Most Favorite Songs
       </h1>
       <div className="flex flex-col items-center md:flex-row md:flex-wrap gap-5">
-        {songs.docs?.map((song) => (
-          <div className="flex flex-col gap-2" key={song.id}>
-            <Image
-              src={`${song.coverImage ? `/media/${song.coverImage.filename}` : `/media/no-picture.jpg`}`}
-              width={200}
-              height={200}
-              alt={song.coverImage ? song.coverImage.alt : "No picture"}
-              className="rounded-sm"
-            />
-            <p>{song.songTitle}</p>
-            <p>{song.artist}</p>
-            <Link
-              href={`/songs/${song.id}`}
-              className="underline hover:text-gray-700 transition-all duration-300"
-            >
-              Go to hear this song
-            </Link>
-          </div>
-        ))}
+        {songs.docs?.map((song) => {
+          const coverImage = song.coverImage as Media;
+          const altImage = coverImage.alt ? coverImage.alt : "no-picture";
+          return (
+            <div className="flex flex-col gap-2" key={song.id}>
+              <Image
+                src={`${coverImage ? `/media/${coverImage.filename}` : `/media/no-picture.jpg`}`}
+                width={200}
+                height={200}
+                alt={altImage}
+                className="rounded-sm"
+              />
+              <p>{song.songTitle}</p>
+              <p>{song.artist}</p>
+              <Link
+                href={`/songs/${song.id}`}
+                className="underline hover:text-gray-700 transition-all duration-300"
+              >
+                Go to hear this song
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
