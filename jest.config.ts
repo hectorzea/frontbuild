5fb2020169ec636e93c180f196038578a1272e77
Héctor Zea <zea3471@gmail.com>
2026-08-04 22:09:17 -0300

    added config type on jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,5 @@
 import nextJest from "next/jest.js";
+import type { Config } from 'jest';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -6,7 +7,7 @@ const createJestConfig = nextJest({
 });
 
 // Add any custom config to be passed to Jest
-const config = {
+const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jest-fixed-jsdom",
   //configuracion de archhivos a ejecutar antes de cada prueba
